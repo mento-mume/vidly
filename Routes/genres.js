b292f53d40b18807a8bd62b6873295c3607a6cec
Mento Mume <akpankevman@gmail.com>
2021-01-29 22:02:10 +0100

    updated work

--- Routes/genres.js
+++ Routes/genres.js
@@ -1,22 +1,21 @@
-
 const Joi = require('joi');
 const mongoose = require('mongoose');
 const express = require('express');
 
 const router = express.Router();
-const genreSchema =  mongoose.Schema({
+
+const Genre = mongoose.model('Genre',new mongoose.Schema({
     name:{
         type:String,
         required:true,
         minlength:5,
         maxlength:50
     }
-});
-const Genre = new mongoose.model('Genre',genreSchema);
+}));
 //get
 router.get('/',async (req,res)=>{
-    const genre = await Genre.find().sort('name');
-    res.send(genre);
+    const genres = await Genre.find().sort('name');
+    res.send(genres);
 });
 
 //post
@@ -24,10 +23,9 @@ router.post('/',async (req,res)=>{
     //validate input
     const {error} = validateGenre(req.body);
     if(error) res.status('400').send(error.details[0].message);
-    
   
     let genre = new Genre({name: req.body.name});
-    genre= await genre.save();
+    genre = await genre.save();
     res.send(genre);
 
 });
@@ -52,10 +50,12 @@ router.delete('/:id',async (req,res)=>{
     res.send(genre);
 });
 
-router.get('/',async (req,res)=>{
+router.get('/id:',async (req,res)=>{
     const genre = await Genre.findById(req.params.id);
+
     if(!genre) return res.status('404').send('the genre with the id could not be found');
-    res.send(genres);
+
+    res.send(genre);
 });
 
 function validateGenre(genre){
